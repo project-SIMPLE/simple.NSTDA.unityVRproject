c75efdaa4d6c85f58f453e08ab78fcc60185ab98
varunyu <varunyu_f@hotmail.com>
2024-10-08 14:37:42 +0700

    Update GAMA Connector script

--- Assets/MyAssets/TempAssets/Script/GAMAConnection/GAMAConnector.cs
+++ Assets/MyAssets/TempAssets/Script/GAMAConnection/GAMAConnector.cs
@@ -4,32 +4,59 @@ using System.Collections.Generic;
 using System.Linq.Expressions;
 using UnityEngine;
 
-public class GAMAConnector : MonoBehaviour
+public class GAMAConnector : SimulationManager
 {
-    // Start is called before the first frame update
-    void Start()
+
+    GAMAMessage_edit message = null;
+    protected override void ManageOtherMessages(string content)
     {
-        
+        // Debug.Log("content " + content);
+        message = GAMAMessage_edit.CreateFromJSON(content);
+         
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void OtherUpdate()
     {
-        
+
     }
+
     public void SendSeedInfoToGAMA(int seedID)
     {
-        Dictionary<string, string> args = new Dictionary<string,string>() { 
-            { "SeedID",seedID.ToString()} 
+
+        Dictionary<string, string> args = new Dictionary<string, string> {
+         {"player_ID", GetTeamID() },
+         {"tree_ID",  seedID.ToString()}
         };
+
+        /*Dictionary<string, string> args = new Dictionary<string,string>() { 
+            { "SeedID",seedID.ToString()} 
+        };*/
 
         try
         {
-            //ConnectionManager.Instance.SendExecutableAsk("", args);
+            ConnectionManager.Instance.SendExecutableAsk("collect_seeds", args);
         }
         catch (Exception e)
         {
             Debug.Log(e);
         }
     }
+
+    private string GetTeamID()
+    {
+        return ConnectionManager.Instance.getUseMiddleware() ? ConnectionManager.Instance.GetConnectionId() : ("\"" + ConnectionManager.Instance.GetConnectionId() + "\""); ;
+    }
+}
+
+[System.Serializable]
+public class GAMAMessage_edit
+{
+    public string Head;
+    public string message;
+
+    public static GAMAMessage_edit CreateFromJSON(string jsonString)
+    {
+        return JsonUtility.FromJson<GAMAMessage_edit>(jsonString);
+    }
+
 }
