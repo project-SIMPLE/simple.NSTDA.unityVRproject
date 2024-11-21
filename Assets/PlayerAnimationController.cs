fc3f38006bda3162d6d3d564ad4af3fa56cf5fa0
varunyu <varunyu_f@hotmail.com>
2024-11-21 13:08:36 +0700

    Add new player Model with walk/idle animation

---
+++ Assets/PlayerAnimationController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    int Idle = Animator.StringToHash("Idle");
+    int Walk = Animator.StringToHash("Walk");
+
+    private Vector3 previousPos;
+    [SerializeField]
+    private bool isWalk;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        previousPos = this.transform.position;
+        isWalk = false;
+        SetPlayerAnimation(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private Vector3 currentPos;
+    private float distance;
+    private void FixedUpdate()
+    {
+        if(anim == null) return;
+        
+        currentPos = this.transform.position;
+        distance = Mathf.Abs(Vector3.Distance(currentPos, previousPos));
+        previousPos = currentPos;
+        if(!isWalk && distance > 0f)
+        {
+            SetPlayerAnimation(1);
+            isWalk = true;
+        }
+        else if(isWalk && distance == 0f)
+        {
+            SetPlayerAnimation(0);
+            isWalk = false;
+        }
+          
+        
+    }
+    public void SetPlayerAnimation(int id)
+    {
+        switch (id)
+        {
+            case 0:
+                anim.SetTrigger(Idle);
+                break;
+            case 1:
+                anim.SetTrigger(Walk);
+                break;
+            
+            default:
+                anim.SetTrigger(Idle);
+                break;
+        }
+    }
+}
